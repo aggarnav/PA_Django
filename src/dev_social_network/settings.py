3c23bd5ac785e5ba14998182a3d613f5ea5df89f
aggarnav <aggarnav@gmail.com>
2020-05-30 14:26:32 +0530

    Update settings.py

--- src/dev_social_network/settings.py
+++ src/dev_social_network/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(STATIC_ENV_ROOT, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ENV_ROOT, 'media')
 
 STATIC_URL = '/static/'
